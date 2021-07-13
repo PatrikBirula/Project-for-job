295290423d96e8d2543ecf552623936f71d2621c
PatrikBirula <87121256+PatrikBirula@users.noreply.github.com>
2021-07-13 23:27:58 +0200

    Now we can find all commits.

--- src/components/home/HomeSearch.vue
+++ src/components/home/HomeSearch.vue
@@ -3,21 +3,20 @@
 		<input
 			v-model="nick"
 			placeholder="Type Username"
-			@keyup="
-				getUser();
-				getRepos();
-			"
+			@keyup="getUser(), getRepos()"
 			ref="input"
 		/>
+		<div v-if="user.name">
+			<img :src="user.avatar_url" alt="Githuber image" />
+			<h1>{{ user.name }}</h1>
+		</div>
+		<ul>
+			<li v-for="(repo, index) in repos" :key="index">
+				<h2 v-on:click="getCommits(repo)">{{ repo.repoName }}</h2>
+				<p>{{ repo.repoDescription }}</p>
+			</li>
+		</ul>
 	</form>
-	<div v-if="user.name">
-		<img :src="user.avatar_url" alt="Githuber image" />
-		<h1>{{ user.name }}</h1>
-	</div>
-	<div v-for="(repo, index) in repos" :key="index">
-		<h2>{{ repo.name }}</h2>
-		<p>{{ repo.description }}</p>
-	</div>
 </template>
 
 <script>
@@ -27,9 +26,10 @@ import { debounce } from "lodash-es";
 export default {
 	data() {
 		return {
-			nick: "",
-			repos: "",
+			nick: "Inza",
 			user: "",
+			repos: [],
+			value: [],
 		};
 	},
 	methods: {
@@ -48,17 +48,47 @@ export default {
 			axios
 				.get(`https://api.github.com/users/${this.nick}/repos`)
 				.then((response) => {
-					this.repos = response.data;
+					this.repos = [];
+					this.value = [];
+					response.data.forEach((repo) => {
+						this.repos.push(this.extractData(repo));
+						this.value.push(this.extractDataName(repo));
+					});
 				})
 				.catch((error) => {
 					console.log(error);
 				});
 		}, 500),
-	},
-	mounted() {
-		this.$refs.input.focus();
+		findMe(repo) {
+			console.log(repo.repoName);
+		},
+		extractData({ name: repoName, description: repoDescription, id: idM }) {
+			return { repoName, repoDescription, idM };
+		},
+		extractDataName({ name: repoName }) {
+			return { repoName };
+		},
+
+		getCommits(repo) {
+			console.log(repo.repoName);
+			axios
+				.get(
+					`https://api.github.com/repos/${this.nick}/${repo.repoName}/commits`
+				)
+				.then((response) => {
+					this.arrayOne = response.data;
+					console.log(this.arrayOne);
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+		},
 	},
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+li {
+	list-style-type: none;
+}
+</style>
